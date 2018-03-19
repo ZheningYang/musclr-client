072e7d04d198774aaa298d406b2eb3fc8eca96c7
Chris <chris_holc@hotmail.fr>
2018-03-19 23:02:18 +0100

    bla

--- src/app/components/scheduler-container/scheduler-drawer/scheduler-drawer.component.ts
+++ src/app/components/scheduler-container/scheduler-drawer/scheduler-drawer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-scheduler-drawer',
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SchedulerDrawerComponent implements OnInit {
 
-  constructor() { }
+  public showFiller = false;
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
