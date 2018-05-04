590d7935f9ef963ae126e0637e7ecb6dbcdf2ec6
ChrisHLC <chris_holc@hotmail.fr>
2018-05-04 16:59:47 +0200

    planning start

--- src/app/components/scheduler-container/scheduler/scheduler.component.ts
+++ src/app/components/scheduler-container/scheduler/scheduler.component.ts
@@ -1,9 +1,13 @@
-import {AfterViewInit, Component, ElementRef, ViewChild, ViewEncapsulation} from '@angular/core';
 import 'dhtmlx-scheduler';
-import {} from '@types/dhtmlxscheduler';
 import 'dhtmlx-scheduler/codebase/locale/locale_fr.js';
+import 'dhtmlx-scheduler/codebase/ext/dhtmlxscheduler_editors.js';
+import {} from '@types/dhtmlxscheduler';
+
+import {AfterViewInit, Component, ElementRef, ViewChild, ViewEncapsulation} from '@angular/core';
 import {SchedulerService} from '../scheduler.service';
 import {Event} from '../../../models/event.model';
+
+// import {} from '@types/dhtmlxscheduler'; is mandatory
 
 @Component({
   encapsulation: ViewEncapsulation.None,
@@ -15,6 +19,43 @@ export class SchedulerComponent implements AfterViewInit {
 
   @ViewChild('scheduler_here')
   schedulerContainer: ElementRef;
+
+  my_workouts = [];
+
+  max_participant_number = [];
+
+  alert_opts = [
+    {key: 1, label: 'None'},
+    {key: 2, label: 'On start date'},
+    {key: 3, label: '1 day before'}
+  ];
+
+  friend_list = [];
+
+  fieldConfig = {
+    myEvent: [
+      {name: 'Titre', height: 30, type: 'textarea', map_to: 'text', focus: true},
+      {name: 'Séance', height: 50, type: 'select', map_to: 'workout', options: this.my_workouts},
+      {name: 'time', height: 72, type: 'time', map_to: 'auto'},
+      {name: 'template', height: 40, type: 'template', map_to: 'my_template'},
+      {name: 'Maximum de participants ', height: 50, type: 'combo', map_to: 'max_participant_number', options: this.max_participant_number}
+    ],
+    addFriends: [
+      {name: 'Amis', height: 50, type: 'combo', map_to: 'friend_list', options: this.friend_list},
+      {name: 'Participants', height: 1000, type: 'participant_template', map_to: 'participant_list'}
+    ],
+    default: [{name: 'text', height: 50, type: 'textarea', map_to: 'text', focus: true},
+      {name: 'Séance', height: 50, type: 'select', map_to: 'workout', options: this.my_workouts},
+      {name: 'Je participe', height: 40, type: 'checkbox', map_to: 'participation'},
+      {name: 'Maximum de participants', height: 40, type: 'select', map_to: 'type', options: this.max_participant_number},
+      {name: 'time', height: 72, type: 'time', map_to: 'auto'}
+    ]
+  };
+
+  buttonConfig = {
+    default: ['dhx_delete_btn', 'custom_btn_invite'],
+    addFriends: ['dhx_delete_btn', 'custom_btn_retour']
+  };
 
   // method to transform event from the scheduler to the DB
   private static serializeEvent(data: any, insert: boolean = false): Event {
@@ -37,6 +78,10 @@ export class SchedulerComponent implements AfterViewInit {
 
 
   constructor(private schedulerService: SchedulerService) {
+    for (let i = 0; i < 10; i++) {
+      this.max_participant_number.push({key: i + 1, label: (i + 1).toString()});
+    }
+
   }
 
   // PRO-TIP
@@ -58,12 +103,21 @@ export class SchedulerComponent implements AfterViewInit {
 
     scheduler.config.xml_date = '%Y-%m-%d %H:%i';
 
+    scheduler.locale.labels['custom_btn_invite'] = 'Inviter';
+    scheduler.locale.labels['custom_btn_retour'] = 'Retour';
+
+    scheduler.config.lightbox.sections = this.fieldConfig['default'];
+    // scheduler.config.buttons_right = this.buttonConfig['default'];
+
+
     scheduler.init(this.schedulerContainer.nativeElement, new Date());
 
     // loads all the user's events
     this.schedulerService.getForAuthenticatedUser()
       .subscribe(
-        (data: JSON) => scheduler.parse(data, 'json'),
+        (data: JSON) => {
+          scheduler.parse(data, 'json');
+        } ,
         error => console.log(error));
 
 
